a7b672e5886b44e58519757e5099efcce26b4bcf
Hilda Abigél Horváth <hilda.horvath@epfl.ch>
2022-05-05 16:34:54 +0200

    second map

--- docs/map2.js
+++ docs/map2.js
@@ -6,32 +6,32 @@ var projection = d3.geoMercator()
     .scale(800);
     // .rotate([-180,0]);
 
-var svg = d3.select("#container").append("svg")
+var svg2 = d3.select("#container2").append("svg")
     .attr("width", width)
     .attr("height", height);
 
 var path = d3.geoPath()
     .projection(projection);
 
-var g = svg.append("g");
+var g2 = svg2.append("g");
 
 // load and display the World
-d3.json("eu.json").then(function(topology) {
+d3.json("world.json").then(function(topology) {
 
-    g.selectAll("path")
-       .data(topojson.feature(topology, topology.objects.europe)
+    g2.selectAll("path")
+       .data(topojson.feature(topology, topology.objects.land)
            .features)
        .enter().append("path")
        .attr("d", path)
-       .attr('class', 'country');
+       .attr('class', 'country2');
 
 });
 
 var zoom = d3.zoom()
       .scaleExtent([1, 8])
       .on('zoom', function(event) {
-          g.selectAll('path')
+          g2.selectAll('path')
            .attr('transform', event.transform);
 });
 
-svg.call(zoom);
+svg2.call(zoom);
