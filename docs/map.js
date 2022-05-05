967238957bd2011ac40833f3a8cb7883863832ec
Adrien Schurger-Foy <adriensf@gmail.com>
2022-05-05 11:28:29 +0200

    add zoom and pan

--- docs/map.js
+++ docs/map.js
@@ -1,28 +1,36 @@
-// Setting up the svg element for D3 to draw in
-let width = 1000, height = 1000
+var width = 800,
+    height = 500;
 
-let svg = d3.select("#container").append("svg")
+var projection = d3.geoMercator()
+    .center([20, 45])
+    .scale(800);
+    // .rotate([-180,0]);
+
+var svg = d3.select("#container").append("svg")
     .attr("width", width)
-    .attr("height", height)
+    .attr("height", height);
 
-let europeProjection = d3.geoMercator()
-  .center([ 13, 52 ])
-  .scale([ width / 1.5 ])
-  .translate([ width / 2, height / 2 ])
+var path = d3.geoPath()
+    .projection(projection);
 
+var g = svg.append("g");
 
-pathGenerator = d3.geoPath().projection(europeProjection)
-geoJsonUrl = "https://gist.githubusercontent.com/spiker830/3eab0cb407031bf9f2286f98b9d0558a/raw/7edae936285e77be675366550e20f9166bed0ed5/europe_features.json"
+// load and display the World
+d3.json("eu.json").then(function(topology) {
 
+    g.selectAll("path")
+       .data(topojson.feature(topology, topology.objects.europe)
+           .features)
+       .enter().append("path")
+       .attr("d", path);
 
-d3.json(geoJsonUrl).then(geojson => {
-    // Tell D3 to render a path for each GeoJSON feature
-    svg.selectAll("path")
-      .data(geojson.features)
-      .enter()
-      .append("path")
-      .attr("d", pathGenerator) // This is where the magic happens
-      .attr("stroke", "grey") // Color of the lines themselves
-      .attr("fill", "white") // Color uses to fill in the lines
-  })
+});
 
+var zoom = d3.zoom()
+      .scaleExtent([1, 8])
+      .on('zoom', function(event) {
+          g.selectAll('path')
+           .attr('transform', event.transform);
+});
+
+svg.call(zoom);
