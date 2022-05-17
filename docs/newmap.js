340c202fd68be0ed57bc7ba1de5430fd081593ab
Adrien Schurger-Foy <adriensf@gmail.com>
2022-05-17 20:33:43 +0200

    add get visible hotels

--- docs/newmap.js
+++ docs/newmap.js
@@ -1,19 +1,60 @@
 /*New map.*/
 var map = L.map('map').setView([47.811195, 13.033229], 4);
+var markerList;
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox/light-v10',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: 'pk.eyJ1IjoiaGhpbGRhYSIsImEiOiJjbDM4bndzNGowMW9pM2pxbWo5aWdnMTR3In0.7VwzpEqRUIHqyO85CB3xJg'
+  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+  maxZoom: 18,
+  id: 'mapbox/light-v10',
+  tileSize: 512,
+  zoomOffset: -1,
+  accessToken: 'pk.eyJ1IjoiaGhpbGRhYSIsImEiOiJjbDM4bndzNGowMW9pM2pxbWo5aWdnMTR3In0.7VwzpEqRUIHqyO85CB3xJg'
 }).addTo(map);
+
+
+// select visible hotels
+function intersectRect(r1, r2) {
+    return !(r2.left > r1.right ||
+      r2.right < r1.left ||
+      r2.top > r1.bottom ||
+      r2.bottom < r1.top);
+  }
+  
+function getVisibleMarkers() {
+    console.log('called');
+    var cc = map.getContainer();
+    var els = cc.getElementsByClassName('leaflet-marker-icon');
+    var ccRect = cc.getBoundingClientRect();
+    var visibles = [];
+    for (var i = 0; i < els.length; i++) {
+      var el = els.item(i);
+      var elRect = el.getBoundingClientRect();
+      intersectRect(ccRect, elRect) && visibles.push(el);
+    }
+    if (visibles.length > 0) return(visibles);
+}
+
 
 fetch("hotel_loc.geojson")
 .then(function(response) {
 return response.json();
 })
 .then(function(data) {
+// console.log(L.geoJSON(data));
 L.geoJSON(data).addTo(map);
+markerList = getVisibleMarkers();
 });
+
+
+function getVisibleMarkerIDs(){
+  ref = markerList;
+  visible = getVisibleMarkers();
+
+  var IDs = []
+  for (let i = 0; i < visible.length; i++) {
+    const ID = ref.indexOf(visible[i]);
+    IDs.push(ID);
+  }
+  return IDs
+}
+
