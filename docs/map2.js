500ef753049ccff8c7c478768d7ebf5d5fe9c59b
julianschnitzler <32015957+julianschnitzler@users.noreply.github.com>
2022-05-17 13:39:35 +0200

    add zoom and points on world map

--- docs/map2.js
+++ docs/map2.js
@@ -14,6 +14,21 @@ var path2 = d3.geoPath()
     .projection(projection2);
 
 var g2 = svg2.append("g");
+var g3 = svg2.append("g");
+
+var hotel_locs = $.getJSON("hotel_loc.json", function(markers) {
+    g3.selectAll("myCircles")
+    .data(markers)
+    .join("circle")
+        .attr("cx", d => projection2([d.lng, d.lat])[0])
+        .attr("cy", d => projection2([d.lng, d.lat])[1])
+        .attr("r", 5)
+        .attr("class", "circle")
+        .style("fill", "69b3a2")
+        .attr("stroke", "#69b3a2")
+        .attr("stroke-width", 3)
+        .attr("fill-opacity", .4);
+});
 
 // load and display the World
 d3.json("world.json").then(function(topology) {
@@ -27,10 +42,17 @@ d3.json("world.json").then(function(topology) {
 });
 
 var zoom2 = d3.zoom()
-      .scaleExtent([1, 8])
+      .scaleExtent([1, 500])
       .on('zoom', function(event) {
-          g2.selectAll('path')
-           .attr('transform', event.transform);
+          //g2.selectAll('path')
+          // .attr('transform', event.transform);
+          g2.attr('transform', event.transform);
+          //var newX = event.transform.rescale(projection2);
+          //var newY = event.transform.rescaleY(projection2);
+          g3.attr('transform', event.transform);
+          g3.selectAll('circle').attr("r", 5/event.transform.k).attr("stroke-width", 3/event.transform.k);
+          //g3.selectAll("circle").attr("r", 5/event.transform.k);
+          console.log(event.transform);
 });
 
 svg2.call(zoom2);
