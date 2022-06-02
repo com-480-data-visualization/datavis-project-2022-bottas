4b214ba4876d506e5e64afffad222e45f21f6f3b
julianschnitzler <32015957+julianschnitzler@users.noreply.github.com>
2022-06-02 18:49:29 +0200

    change size of lineplot

--- docs/js/lineplot.js
+++ docs/js/lineplot.js
@@ -1,4 +1,4 @@
-let svg_line = d3.select("#lineplot").append("svg").attr("width", 700).attr("height", 300)
+let svg_line = d3.select("#lineplot").append("svg").attr("width", 500).attr("height", 300)
 
 let y_scale = d3.scaleLinear().domain([10,7]).range([50,250]);
 let y_axis = d3.axisRight()
@@ -6,7 +6,7 @@ let y_axis = d3.axisRight()
 svg_line.append("g").attr("transform", "translate(50, 0)").call(y_axis);
 
 let x_scale = d3.scaleTime().domain([new Date(2015, 8, 1), new Date(2017, 8, 31)])
-.range([50, 650]);
+.range([50, 450]);
 let x_axis = d3.axisBottom().scale(x_scale);
 svg_line.append("g").attr("transform", "translate(0, 250)").attr("fill", "black").call(x_axis);
 
@@ -47,16 +47,16 @@ function update_line(city){
 var start_date = new Date(2015, 8, 1);
 var end_date = new Date(2017, 8, 31);
 
-svg_line.append("rectangle").attr("width", 700).attr("height", 300)
+svg_line.append("rectangle").attr("width", 500).attr("height", 300)
 .attr("style", "fill:rgb(255,255,255);stroke-width:3;stroke:rgb(0,0,0)");
 
 const brush = d3.brushX()
-.extent([[50, 50], [650, 250]]);
+.extent([[50, 50], [450, 250]]);
 
 
 brush.on("end", brushended);
 
-svg_line.append("g").call(brush).call(brush.move, [50, 650]);
+svg_line.append("g").call(brush).call(brush.move, [50, 450]);
 
 function brushended(event) {
     const selection = event.selection;
