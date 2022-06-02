45d8b4d084e6a32cbef13b47fa6fb035a5738249
julianschnitzler <32015957+julianschnitzler@users.noreply.github.com>
2022-06-02 23:27:13 +0200

    refactor connection map

--- docs/js/connection_map.js
+++ docs/js/connection_map.js
@@ -1,37 +1,37 @@
+// svg size
 var scale = 0.8
-var width2 = window.innerWidth * scale,
-    height2 = 500 * scale;
+var svg_width = window.innerWidth * scale,
+    svg_height = 500 * scale;
 
-var geo_json = null;
+var geo_json;
+var current_transform = null;
+var hotel_name_to_lonlat = {};
 
-var projection2 = d3.geoMercator()
+var geo_projection = d3.geoMercator()
     .center([20, 45])
     .scale(100);
-    // .rotate([-180,0]);
 
-var svg2 = d3.select("#container2").append("svg")
-    .attr("width", width2)
-    .attr("height", height2);
-
+var map_svg = d3.select("#container2").append("svg")
+    .attr("width", svg_width)
+    .attr("height", svg_height);
 var path2 = d3.geoPath()
-    .projection(projection2);
-
-var g2 = svg2.append("g");
-var g3 = svg2.append("g");
-
-var current_hotel = "Doubletree by Hilton London Kensington";
-var current_transform = null;
-
-var div_hotel = d3.select("body").append("div")
+    .projection(geo_projection);
+var g_countries = map_svg.append("g");
+var g_hotels = map_svg.append("g");
+var hotel_tooltip = d3.select("body").append("div")
      .attr("class", "tooltip-hotel")
      .style("opacity", 0);
-var hotel_name_to_lonlat = {};
-var hotel_locs = $.getJSON("./data/json_world_map/hotel_loc.json", function(markers) {
-    g3.selectAll("myCircles")
+var line_tooltip = d3.select("body").append("div")
+     .attr("class", "tooltip-line")
+     .style("opacity", 0);
+
+// add hotel markings
+$.getJSON("./data/json_world_map/hotel_loc.json", function(markers) {
+    g_hotels.selectAll("myCircles")
     .data(markers)
     .join("circle")
-        .attr("cx", d => projection2([d.lng, d.lat])[0])
-        .attr("cy", d => projection2([d.lng, d.lat])[1])
+        .attr("cx", d => geo_projection([d.lng, d.lat])[0])
+        .attr("cy", d => geo_projection([d.lng, d.lat])[1])
         .attr("r", 5)
         .attr("id", d => d.Hotel_Name)
         .attr("class", "circle")
@@ -41,17 +41,21 @@ var hotel_locs = $.getJSON("./data/json_world_map/hotel_loc.json", function(mark
         .attr("fill-opacity", .4)
         .style('vector-effect', 'non-scaling-stroke')
         .on('click', function(d, i) {
+            // get position of svg on webpage
             let matrix = this.getScreenCTM().translate(+this.getAttribute("cx"),
             +this.getAttribute("cy"));
+
             current_hotel = this.id;
             draw_lines(current_hotel);
             draw_colors(current_hotel);
-            div_hotel.html("Current Hotel:  " + current_hotel).style("opacity", 1)
+
+            hotel_tooltip.html("Current Hotel:  " + current_hotel).style("opacity", 1)
             .style("left", 
                    (window.pageXOffset + matrix.e - this.getAttribute("cx")) + "px")
             .style("top",
                    (window.pageYOffset + matrix.f - this.getAttribute("cy") + 30) + "px");
         });
+
     markers.forEach(function(d) {
         hotel_name_to_lonlat[d.Hotel_Name] = [d.lng, d.lat];  
     })
@@ -59,7 +63,7 @@ var hotel_locs = $.getJSON("./data/json_world_map/hotel_loc.json", function(mark
 
 // load and display the World
 d3.json("./data/json_world_map/countries-110m.json").then(function(topology) {
-    g2.selectAll("path")
+    g_countries.selectAll("path")
        .data(topojson.feature(topology, topology.objects.countries)
            .features)
        .enter().append("path")
@@ -78,14 +82,10 @@ d3.json("./data/json_world_map/countries.geojson").then(function(json) {
     );
 });
 
-var div = d3.select("body").append("div")
-     .attr("class", "tooltip-line")
-     .style("opacity", 0);
-
 
 function draw_lines(hotel) {
 d3.json('./data/json_world_map/reviewer_nationalities.json').then(function(json) {
-    svg2.selectAll('line').remove();
+    map_svg.selectAll('line').remove();
 
     current_hotel_lonlat = hotel_name_to_lonlat[hotel];
 
@@ -93,59 +93,60 @@ d3.json('./data/json_world_map/reviewer_nationalities.json').then(function(json)
         if (d.Hotel_Name == hotel) {
             //console.log(d);
             country_lonlat = country_to_lonlat[d.Reviewer_Nationality];
-            svg2.append("line").attr("x1", projection2(country_lonlat)[0])
-            .attr("y1", projection2(country_lonlat)[1])
-            .attr("x2", projection2(current_hotel_lonlat)[0])
-            .attr("y2", projection2(current_hotel_lonlat)[1])
+            map_svg.append("line").attr("x1", geo_projection(country_lonlat)[0])
+            .attr("y1", geo_projection(country_lonlat)[1])
+            .attr("x2", geo_projection(current_hotel_lonlat)[0])
+            .attr("y2", geo_projection(current_hotel_lonlat)[1])
             .style('stroke', 'black').style('stroke-width',  Math.log(0.2*d.Number)).style('vector-effect', 'non-scaling-stroke')
             .attr('id', d.Reviewer_Nationality).attr('no_reviewers', d.Number).attr('transform', current_transform);
         }
     });
 
-    let lines = svg2.selectAll('line');
+    let lines = map_svg.selectAll('line');
     
     lines.on('mouseover', function (d, i) {
         let line = d3.select(this);
         d3.select(this).transition()
              .duration('50')
              .attr('opacity', '.85');
-        div.transition()
+        line_tooltip.transition()
             .duration(50)
             .style("opacity", 1);
-        div.html(this.id + ': ' + line.attr('no_reviewers')).style("left", (d.pageX + 10) + "px")
+        line_tooltip.html(this.id + ': ' + line.attr('no_reviewers')).style("left", (d.pageX + 10) + "px")
         .style("top", (d.pageY - 15) + "px");
     })
    .on('mouseout', function (d, i) {
         d3.select(this).transition()
              .duration('50')
              .attr('opacity', '1');
-        div.transition()
+        line_tooltip.transition()
              .duration('50')
              .style("opacity", 0);    
-});
-});
+        });
+    });
 
-svg2.call(zoom2);
+    map_svg.call(zoom2);
 };
 
+// change colors based on number of reviewers from that country
 function draw_colors(hotel) {
-d3.json('./data/json_world_map/reviewer_nationalities.json').then(function(json) {
-    var myColor = d3.scaleSequential().domain([0,6]).interpolator(d3.interpolateViridis);    
-        
-                g2.selectAll("path")
-                    .style("fill", function(g){
-                        for (const d of json) {
-                            if (d.Hotel_Name == hotel) {
-                                if (d.Reviewer_Nationality == g.properties.name){
-                                    console.log(myColor(d.Number));
-                                    return myColor(Math.log(d.Number));
-                                    };    
-                                } 
-                        };
-                        return myColor(0);
-                    })
-})
-svg2.call(zoom2);
+    d3.json('./data/json_world_map/reviewer_nationalities.json').then(function(json) {
+        var myColor = d3.scaleSequential().domain([0,6]).interpolator(d3.interpolateViridis);    
+            
+                    g_countries.selectAll("path")
+                        .style("fill", function(g){
+                            for (const d of json) {
+                                if (d.Hotel_Name == hotel) {
+                                    if (d.Reviewer_Nationality == g.properties.name){
+                                        console.log(myColor(d.Number));
+                                        return myColor(Math.log(d.Number));
+                                        };    
+                                    } 
+                            };
+                            return myColor(0);
+                        })
+        })
+    map_svg.call(zoom2);
 };
 
 
@@ -153,24 +154,12 @@ svg2.call(zoom2);
 var zoom2 = d3.zoom()
       .scaleExtent([1, 500])
       .on('zoom', function(event) {
-          //g2.selectAll('path')
-          // .attr('transform', event.transform);
-          g2.attr('transform', event.transform);
-          //var newX = event.transform.rescale(projection2);
-          //var newY = event.transform.rescaleY(projection2);
-          g3.attr('transform', event.transform);
+          g_countries.attr('transform', event.transform);
+          g_hotels.attr('transform', event.transform);
           const new_r = 5/event.transform.k;
-          g3.selectAll('circle').attr("r", new_r);
-          svg2.selectAll("line").attr('transform', event.transform);
+          g_hotels.selectAll('circle').attr("r", new_r);
+          map_svg.selectAll("line").attr('transform', event.transform);
           current_transform = event.transform;
-          //g3.selectAll("circle").attr("r", 5/event.transform.k);
 });
 
-//draw_lines(current_hotel);
-//draw_colors(current_hotel);
-svg2.call(zoom2);
-
-//let hotels = g3.selectAll("*");
-//for (const hotel of hotels) {
-//    console.log(hotel);
-//}
+map_svg.call(zoom2);
