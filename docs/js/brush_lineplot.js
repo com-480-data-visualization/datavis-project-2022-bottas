998dd9a87184f292987cfd163ddc581b2a46c9ca
julianschnitzler <32015957+julianschnitzler@users.noreply.github.com>
2022-06-02 23:31:01 +0200

    refactor lineplot

--- docs/js/brush_lineplot.js
+++ docs/js/brush_lineplot.js
@@ -1,21 +1,15 @@
+var start_date = new Date(2015, 8, 1);
+var end_date = new Date(2017, 8, 31);
+
 let svg_line = d3.select("#lineplot").append("svg").attr("width", 500).attr("height", 300)
 
 let y_scale = d3.scaleLinear().domain([10,7]).range([50,250]);
-let y_axis = d3.axisRight()
-.scale(y_scale);
+let y_axis = d3.axisRight().scale(y_scale);
 svg_line.append("g").attr("transform", "translate(50, 0)").call(y_axis);
 
-let x_scale = d3.scaleTime().domain([new Date(2015, 8, 1), new Date(2017, 8, 31)])
-.range([50, 450]);
+let x_scale = d3.scaleTime().domain([new Date(2015, 8, 1), new Date(2017, 8, 31)]).range([50, 450]);
 let x_axis = d3.axisBottom().scale(x_scale);
 svg_line.append("g").attr("transform", "translate(0, 250)").attr("fill", "black").call(x_axis);
-
-//svg_line.append("line").style("stroke", "black")
-//.style("stroke-width", 1.5)
-//.attr("x1", 50)
-//.attr("y1", 100)
-//.attr("x2", 650)
-//.attr("y2", 100);
 
 function update_line(city){
     svg_line.selectAll("path").remove();
@@ -41,19 +35,11 @@ function update_line(city){
     current_lineplot_city = city;
 }
 
-//update_line("whole");
-
-
-var start_date = new Date(2015, 8, 1);
-var end_date = new Date(2017, 8, 31);
-
 svg_line.append("rectangle").attr("width", 500).attr("height", 300)
 .attr("style", "fill:rgb(255,255,255);stroke-width:3;stroke:rgb(0,0,0)");
 
 const brush = d3.brushX()
 .extent([[50, 50], [450, 250]]);
-
-
 brush.on("end", brushended);
 
 svg_line.append("g").call(brush).call(brush.move, [50, 450]);
